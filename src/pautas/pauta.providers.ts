e9630bace5a0d4acf44bb228cf31584dd8b74ae9
CristianoSFMothe <cristianodevsystem@gmail.com>
2023-10-16 17:28:34 -0300

    :up: Criação do providers

---
+++ src/pautas/pauta.providers.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { DataSource, Repository } from 'typeorm';
+import { Pauta } from './pauta.entity';
+import { Provider } from '@nestjs/common';
+
+const PautaRepository: Provider<Repository<Pauta>> = {
+  provide: 'PAUTA_REPOSITORY',
+  useFactory: (dataSource: DataSource) => {
+    const repository: Repository<Pauta> = dataSource.getRepository(Pauta);
+
+    return repository;
+  },
+
+  inject: ['DATA_SOURCE'],
+};
+
+export const pautaProviders: Provider[] = [PautaRepository];
